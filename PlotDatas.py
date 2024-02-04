02b1743a6c0fb56882a3529ef32935fdbc49618f
WolfgangPhy <antonin937@gmail.com>
2024-02-04 08:53:37 +0100

    VictoryVsBubbleSize

--- PlotDatas.py
+++ PlotDatas.py
@@ -45,10 +45,26 @@ def PlotVictoryVsOvoidity(data):
     ax.set_xlabel('Victories')
     ax.set_ylabel('Ovoidity (Height/Width)')
     plt.savefig('VictoriesVsOvoidity.png', dpi=300, bbox_inches='tight')
+  
+def PlotVictoryVsBubbleSize(data):
+    """
+    Plots the Victories vs Bubble Size and saves the plot as a png file.
+    Args:
+        data (pandas dataframe): Dataframe containing the datas of FPT_Eggs.csv
+    """
+    sns.set_theme()
+    fig, ax = plt.subplots()
+    sns.lineplot(x='Victories', y='Bubble', data=data, ax=ax)
+    ax.set_title('Victories vs Bubble Size')
+    ax.set_xlabel('Victories')
+    ax.set_ylabel('Bubble Size (mm)')
+    plt.savefig('VictoriesVsBubbleSize.png', dpi=300, bbox_inches='tight')
+    #plt.show()
     
+      
 def main():
     data = pd.read_csv('FPT_Eggs.csv')
-    PlotVictoryVsOvoidity(data)
+    PlotVictoryVsBubbleSize(data)
     
 if __name__ == '__main__':
     main()
